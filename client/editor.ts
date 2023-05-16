380ec1a035b94379042f53d21a912db3a20d775d
9ziggy9 <davidarogers@protonmail.com>
2023-05-16 14:45:16 -0700

    editor text grabbing functional

--- client/editor.ts
+++ client/editor.ts
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
+function handleEditorInput(editor: HTMLElement | null,
+                           output: HTMLElement | null): void {
+  if (!editor || !output) throw new Error("Null DOM input.")
+  editor.addEventListener("input", (e) => {
+    output.innerHTML = (e.target as HTMLInputElement).value;
+  });
+}
+
+function main(): void {
   console.log("Hello, editor!");
-});
+  const editor = document.getElementById("text-editor");
+  const output = document.getElementById("text-output");
+  handleEditorInput(editor, output);
+}
+
+document.addEventListener("DOMContentLoaded", main);
