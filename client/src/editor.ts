83b65c7185c51225373c49e23ef970f5ac54bd94
9ziggy9 <davidarogers@protonmail.com>
2023-05-17 12:55:34 -0700

    closable modal

--- client/src/editor.ts
+++ client/src/editor.ts
@@ -1,8 +1,23 @@
 import katex from "katex";
 import { symbolTable } from "./symbols"
 
+const listenModalClose: (element: Element | null) => void
+  = (el) => {
+    if (!el) throw new Error("Overlay node is NULL");
+    el.addEventListener("click", () => {
+      el.classList.toggle("hidden");
+    });
+  };
+
+const listenSearchClick: (element: Element | null) => void
+  = (el) => {
+    if (!el) throw new Error("Search area node is NULL");
+    el.addEventListener("click", (e) => e.stopPropagation());
+  };
+
 function handleEditorInput(editor: HTMLElement | null,
-                           output: HTMLElement | null): void {
+                           output: HTMLElement | null): void
+{
   if (!editor || !output) throw new Error("Null DOM input.")
   editor.addEventListener("input", (e) => {
     const input: string = (e.target as HTMLInputElement).value;
@@ -36,10 +51,14 @@ function setExampleText(
 function main(): void {
   console.log("Hello, editor!");
   console.log(symbolTable);
-  const editor = document.getElementById("text-editor");
-  const output = document.getElementById("text-output");
+  const editor    = document.getElementById("text-editor");
+  const output    = document.getElementById("text-output");
+  const modal     = document.querySelector(".modal-overlay");
+  const symSearch = document.querySelector(".modal-symbol-ctr")
   handleEditorInput(editor, output);
   setExampleText(editor, output);
+  listenModalClose(modal);
+  listenSearchClick(symSearch);
 }
 
 document.addEventListener("DOMContentLoaded", main);
